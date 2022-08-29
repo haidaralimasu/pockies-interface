2ce4a981f54938d8553315d42630b5038f90b403
Haidarali Masu <haidaralimasu123@gmail.com>
2022-08-29 22:22:12 +0530

    changed font

--- src/components/Minter/index.js
+++ src/components/Minter/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import {
   useMaxSupply,
   useTotalSupply,
@@ -7,108 +7,110 @@ import {
   usePrice,
   useTotalMinted,
   usePresale,
-} from '../../hooks/useContract'
-import { useEthers } from '@usedapp/core'
-import WalletConnectButton from '../WalletConnectButton'
-import { ethers } from 'ethers'
-import { expressUrl } from '../../config'
-import axios from 'axios'
-import { pockiesContract } from '../../utils'
-import { notifySuccess, notifyError } from '../toast'
+} from "../../hooks/useContract";
+import { useEthers } from "@usedapp/core";
+import WalletConnectButton from "../WalletConnectButton";
+import { ethers } from "ethers";
+import { expressUrl } from "../../config";
+import axios from "axios";
+import { pockiesContract } from "../../utils";
+import { notifySuccess, notifyError } from "../toast";
 
 const Minter = () => {
-  const { account } = useEthers()
-  const userAccount = account ? account : ethers.constants.AddressZero
+  const { account } = useEthers();
+  const userAccount = account ? account : ethers.constants.AddressZero;
 
-  const totalSupply = useTotalSupply()
-  const formattedTotalSupply = totalSupply ? totalSupply.toNumber() : 0
-  const maxSupply = useMaxSupply()
-  const formattedMaxSupply = maxSupply ? maxSupply.toNumber() : 10000
-  const price = usePrice()
-  const formattedPrice = price ? price.toString() : 0
-  const maxPockiesPerTx = useMaxPockiesPerTx()
+  const totalSupply = useTotalSupply();
+  const formattedTotalSupply = totalSupply ? totalSupply.toNumber() : 0;
+  const maxSupply = useMaxSupply();
+  const formattedMaxSupply = maxSupply ? maxSupply.toNumber() : 10000;
+  const price = usePrice();
+  const formattedPrice = price ? price.toString() : 0;
+  const maxPockiesPerTx = useMaxPockiesPerTx();
   const formattedMaxPockiePerTx = maxPockiesPerTx
     ? maxPockiesPerTx.toNumber()
-    : 0
-  const maxPockiesPerWallet = useMaxPockiesPerWallet()
+    : 0;
+  const maxPockiesPerWallet = useMaxPockiesPerWallet();
   const formattedMaxPockiesPerWallet = maxPockiesPerWallet
     ? maxPockiesPerWallet.toNumber()
-    : 0
-  const totalMinted = useTotalMinted(userAccount)
-  const formattedTotalMinted = totalMinted ? totalMinted.toNumber() : 0
-  const isPresale = usePresale()
-  const formattedIsPresale = isPresale ? isPresale : false
+    : 0;
+  const totalMinted = useTotalMinted(userAccount);
+  const formattedTotalMinted = totalMinted ? totalMinted.toNumber() : 0;
+  const isPresale = usePresale();
+  const formattedIsPresale = isPresale ? isPresale : false;
 
-  const [hexProof, setHexProof] = useState([])
-  const [amount, setAmount] = useState(1)
-  const [waiting, setWaiting] = useState(false)
+  const [hexProof, setHexProof] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [waiting, setWaiting] = useState(false);
 
   const incrementAmount = () => {
     if (amount < formattedMaxPockiePerTx) {
-      setAmount(amount + 1)
+      setAmount(amount + 1);
     }
-  }
+  };
 
   const decrementAmount = () => {
     if (amount > 1) {
-      setAmount(amount - 1)
+      setAmount(amount - 1);
     }
-  }
+  };
 
   const getHexProof = async () => {
     try {
-      const res = await axios.get(`${expressUrl}/${userAccount}`)
-      setHexProof(res.data)
+      const res = await axios.get(`${expressUrl}/${userAccount}`);
+      setHexProof(res.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const whitelistMint = async () => {
     try {
-      setWaiting(true)
-      const txCost = Number(formattedPrice) * amount
+      setWaiting(true);
+      const txCost = Number(formattedPrice) * amount;
       const tx = await pockiesContract.mintPreSale(amount, hexProof, {
         value: txCost.toString(),
-      })
-      await tx.wait()
-      setWaiting(false)
-      await notifySuccess('You have succesfully Minted Pockie')
-      setWaiting(false)
+      });
+      await tx.wait();
+      setWaiting(false);
+      await notifySuccess("You have succesfully Minted Pockie");
+      setWaiting(false);
     } catch (error) {
-      setWaiting(false)
-      notifyError('Something Went Wrong')
-      console.log(error)
+      setWaiting(false);
+      notifyError("Something Went Wrong");
+      console.log(error);
     }
-  }
+  };
 
   const mint = async () => {
     try {
-      setWaiting(true)
-      const txCost = Number(formattedPrice) * amount
+      setWaiting(true);
+      const txCost = Number(formattedPrice) * amount;
       const tx = await pockiesContract.mintPublicSale(amount, {
         value: txCost.toString(),
-      })
-      await tx.wait()
-      await notifySuccess('You have succesfully Minted Pockie')
-      setWaiting(false)
+      });
+      await tx.wait();
+      await notifySuccess("You have succesfully Minted Pockie");
+      setWaiting(false);
     } catch (error) {
-      setWaiting(false)
-      notifyError('Something Went Wrong')
-      console.log(error)
+      setWaiting(false);
+      notifyError("Something Went Wrong");
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     if (account && formattedIsPresale == true) {
-      getHexProof()
+      getHexProof();
     }
-  }, [hexProof])
+  }, [hexProof]);
+
+  console.log(hexProof);
 
   return (
     <>
       <div className="minter-main">
-        <h1 style={{ color: 'white' }} className="minter-h1">
+        <h1 style={{ color: "white" }} className="minter-h1">
           Pockies Mint Is Live
         </h1>
 
@@ -118,7 +120,7 @@ const Minter = () => {
               <div className="minting-section">
                 <button className="connect btn btn-gradient-blue">{`${account.slice(
                   0,
-                  6,
+                  6
                 )}...${account.slice(-6)}`}</button>
 
                 {formattedIsPresale ? (
@@ -133,10 +135,10 @@ const Minter = () => {
                       onClick={() => whitelistMint()}
                       className="btn mint  btn-gradient-blue"
                     >
-                      {waiting ? 'Please Wait' : `Mint ${amount}`}
+                      {waiting ? "Please Wait" : `Mint ${amount}`}
                     </button>
                     <button
-                      style={{ textAlign: 'center' }}
+                      style={{ textAlign: "center" }}
                       className="btn btn-round amount  btn-gradient-blue"
                       onClick={() => incrementAmount()}
                     >
@@ -155,7 +157,7 @@ const Minter = () => {
                       onClick={() => mint()}
                       className="btn mint  btn-gradient-blue"
                     >
-                      {waiting ? 'Please Wait' : `Mint ${amount}`}
+                      {waiting ? "Please Wait" : `Mint ${amount}`}
                     </button>
                     <button
                       className="btn btn-round amount  btn-gradient-blue"
@@ -185,7 +187,7 @@ const Minter = () => {
             <div className="minter-status-card">
               <h6>Status</h6>
 
-              <h2>{formattedIsPresale ? 'Presale' : 'Publicsale'}</h2>
+              <h2>{formattedIsPresale ? "Presale" : "Publicsale"}</h2>
             </div>
             <div className="vl"></div>
             <div className="minter-status-card">
@@ -229,7 +231,7 @@ const Minter = () => {
         )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Minter
+export default Minter;
